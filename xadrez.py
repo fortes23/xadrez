16fa904c4a08dcb41923267512d64b78e0e24294
fortes23 <brais.fortes@gmail.com>
2020-01-09 18:56:09 +0100

    feat: Use stdin and stdout to work with bots

--- xadrez.py
+++ xadrez.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
 
 import chess
 
@@ -7,18 +11,29 @@ board = chess.Board()
 
 print("Starting to work...\n")
 
-while not board.is_checkmate():
-    found = False
-    print(board)
-    while not found:
-        turn = 'WHITE' if board.turn == chess.WHITE else 'BLACK'
-        a = input(f"\nInput code ({turn})\n")
-        if a[0:2] not in chess.SQUARE_NAMES or a[2:4] not in chess.SQUARE_NAMES:
-            print('Invalid uci code')
-            continue
+proc1 = subprocess.Popen(['python3', './bots/randombot.py'],
+                        stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
 
-        if chess.Move.from_uci(a) in list(board.legal_moves):
-            board.push_uci(a)
-            found = True
-        else:
-            print('Invalid code movement')
+proc2 = subprocess.Popen(['python3', './bots/randombot.py'],
+                        stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+
+while not board.is_game_over():
+    print(board)
+    fen = board.fen() + '\n'
+
+    if board.turn:
+        proc = proc1
+    else:
+        proc = proc2
+
+    os.write(proc.stdin.fileno(), fen.encode())
+    outs = os.read(proc.stdout.fileno(), 4096)
+    errs = os.read(proc.stderr.fileno(), 4096)
+
+    move = outs.decode('utf-8').replace('\n', '')
+
+    print(move)
+    print(errs)
+    board.push_uci(move)
