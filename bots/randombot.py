35a115af797ca110c362901dee337c3eae943391
fortes23 <brais.fortes@gmail.com>
2020-01-09 18:54:42 +0100

    chore: Remove unnecessary code in randombot

--- bots/randombot.py
+++ bots/randombot.py
@@ -5,18 +5,9 @@ import sys
 import random
 
 import chess
-#from ..xadrez.util import debug, move
 
-# this must be moved to util.py
 def debug(msg):
     print(msg, file=sys.stderr)
-
-def move(mov):
-    if len(mov) !=4 or mov[0:2] not in chess.SQUARE_NAMES or mov[2:4] not in chess.SQUARE_NAMES:
-        debug('Invalid uci move')
-    else:
-        print(mov)
-###
 
 debug("Random bot starting!\n")
 
@@ -26,4 +17,4 @@ while True:
     debug("fen: " + board.fen())
     moves = list(board.legal_moves)
     r = random.randint(0, len(moves)-1)
-    move(moves[r].uci())
+    print(moves[r].uci())
