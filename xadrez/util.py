0278072f017b752a8e3ea1b3f9caa5a5a2f52e00
gdalbeniz <gdalbeniz@gmail.com>
2020-01-09 08:07:05 +0000

    feat: initial util file

---
+++ xadrez/util.py
@@ -0,0 +1,10 @@
+import sys
+
+def debug(msg):
+    print(msg, file=sys.stderr)
+
+def move(mov):
+    if len(mov) !=4 or mov[0:2] not in chess.SQUARE_NAMES or mov[2:4] not in chess.SQUARE_NAMES:
+        debug('Invalid uci move')
+    else:
+        print(mov)
