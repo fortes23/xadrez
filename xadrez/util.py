5a7602735332262f4983ddb5fc38cc4dec75d3d5
fortes23 <brais.fortes@gmail.com>
2020-01-09 19:00:19 +0100

    fix: Remove move function from util

--- xadrez/util.py
+++ xadrez/util.py
@@ -1,12 +1,5 @@
 import sys
 
-import chess
 
 def debug(msg):
     print(msg, file=sys.stderr)
-
-def move(mov):
-    if len(mov) !=4 or mov[0:2] not in chess.SQUARE_NAMES or mov[2:4] not in chess.SQUARE_NAMES:
-        debug('Invalid uci move')
-    else:
-        print(mov)
