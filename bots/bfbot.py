d19cfc7a20b1112cbbbc7b12350170ef272ba1f4
fortes23 <brais.fortes@gmail.com>
2020-01-13 20:00:34 +0100

    bots: Create new bot (BF)
    Bot is using minimax strategy, evaluating the values of the pieces

---
+++ bots/bfbot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import random
+
+import chess
+
+
+def debug(msg):
+    print(msg, file=sys.stderr)
+
+
+debug("BF bot starting!\n")
+
+
+weight_pieces = {
+    'p': 1,
+    'n': 3,
+    'b': 3,
+    'r': 5,
+    'q': 9,
+    'k': 90
+}
+
+
+def get_piece_val(board, sq):
+    p = board.piece_at(sq)
+    if p:
+        if p.color:
+            return weight_pieces[str(p).lower()]
+        else:
+            return -weight_pieces[str(p).lower()]
+    return 0
+
+
+def eval_fn(board):
+    res = 0
+    for sq in chess.SQUARES:
+        res += get_piece_val(board, sq)
+    return res
+
+
+def minimax(board, depth, _maximize):
+
+    if board.is_checkmate():
+        return ((-1)**_maximize * 10000, None)
+    elif board.is_stalemate() or board.is_insufficient_material():
+        return(0, None)
+
+    if not depth:
+        return (eval_fn(board), None)
+
+    best_score = (-1)**_maximize * 9999999
+
+    res = None
+    legal_moves = list(board.legal_moves)
+    random.shuffle(legal_moves)
+    for m in legal_moves:
+        aux_b = board.copy()
+        aux_b.push_uci(m.uci())
+        sub_score = minimax(aux_b, depth-1, not _maximize)[0]
+        if _maximize:
+            sb = sub_score - best_score
+        else:
+            sb = best_score - sub_score
+
+        if sb > 0:
+            best_score = sub_score
+            res = m.uci()
+
+    return (best_score, res)
+
+
+while True:
+    fen = input()
+    board = chess.Board(fen=fen)
+
+    debug("fen: " + board.fen())
+    moves = list(board.legal_moves)
+
+    # Get best movement
+    (best, m) = minimax(board, 2, board.turn)
+    debug(f"Best played {best}")
+
+    print(m)
