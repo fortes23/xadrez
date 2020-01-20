0a6a5ebb86ff72bd005da366dcd98533ac60da8c
fortes23 <brais.fortes@gmail.com>
2020-01-20 19:25:11 +0100

    bots: Create and use Xouba class from Fish

--- bots/xouba/xouba.py
+++ bots/xouba/xouba.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import random
+import os
 
 import chess
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../../xadrez')
 
-def debug(msg):
-    print(msg, file=sys.stderr)
+from fish import Fish  # noqa: E402
 
-
-debug("BF bot starting!\n")
 
 pawn_weight_pos = [0, 0, 0, 0, 0, 0, 0, 0,
                    5, 10, 10, -20, -20, 10, 10, 5,
@@ -67,76 +65,34 @@ kings_weight_pos = [20, 30, 10, 0, 0, 10, 30, 20,
                     -30, -40, -40, -50, -50, -40, -40, -30,
                     -30, -40, -40, -50, -50, -40, -40, -30]
 
-weight_pieces = {
-    'p': {"mat": 10, "pos": pawn_weight_pos},
-    'n': {"mat": 30, "pos": knights_weight_pos},
-    'b': {"mat": 30, "pos": bishops_weight_pos},
-    'r': {"mat": 50, "pos": rooks_weight_pos},
-    'q': {"mat": 90, "pos": queens_weight_pos},
-    'k': {"mat": 900, "pos": kings_weight_pos}
-}
+
+class Xouba(Fish):
+    def __init__(self, depth):
+        super().__init__(depth, "Starting xouba!")
+
+        self.weight_pieces = {
+            'p': {"mat": 10, "pos": pawn_weight_pos},
+            'n': {"mat": 30, "pos": knights_weight_pos},
+            'b': {"mat": 30, "pos": bishops_weight_pos},
+            'r': {"mat": 50, "pos": rooks_weight_pos},
+            'q': {"mat": 90, "pos": queens_weight_pos},
+            'k': {"mat": 900, "pos": kings_weight_pos}
+        }
+
+    def get_weigth_piece(self, name, pos):
+        return self.weight_pieces[name]['mat'] + self.weight_pieces[name]['pos'][pos]
+
+    def eval_fn(self, board):
+        res = 0
+        for p in chess.PIECE_TYPES:
+            for wp in board.pieces(p, chess.WHITE):
+                res += self.get_weigth_piece(chess.piece_symbol(p), wp)
+            for bp in board.pieces(p, chess.BLACK):
+                res -= self.get_weigth_piece(chess.piece_symbol(p), chess.square_mirror(bp))
+
+        return res
 
 
-def get_weigth_piece(name, pos):
-    return weight_pieces[name]['mat'] + weight_pieces[name]['pos'][pos]
-
-
-def eval_fn(board):
-    res = 0
-    for p in chess.PIECE_TYPES:
-        for wp in board.pieces(p, chess.WHITE):
-            res += get_weigth_piece(chess.piece_symbol(p), wp)
-        for bp in board.pieces(p, chess.BLACK):
-            res -= get_weigth_piece(chess.piece_symbol(p), chess.square_mirror(bp))
-
-    return res
-
-
-def minimax(board, depth, _maximize, alpha, beta):
-
-    if board.is_checkmate():
-        return ((-1)**_maximize * 10000, None)
-    elif board.is_stalemate() or board.is_insufficient_material():
-        return(0, None)
-
-    if not depth:
-        return (eval_fn(board), None)
-
-    best_score = (-1)**_maximize * 9999999
-
-    res = None
-    legal_moves = list(board.legal_moves)
-    random.shuffle(legal_moves)
-    for m in legal_moves:
-        aux_b = board.copy()
-        aux_b.push_uci(m.uci())
-        sub_score = minimax(aux_b, depth-1, not _maximize, alpha, beta)[0]
-        if _maximize:
-            alpha = max(alpha, sub_score)
-            sb = sub_score - best_score
-        else:
-            beta = min(beta, sub_score)
-            sb = best_score - sub_score
-
-        if sb > 0:
-            best_score = sub_score
-            res = m.uci()
-
-        if alpha >= beta:
-            break
-
-    return (best_score, res)
-
-
+x = Xouba(3)
 while True:
-    fen = input()
-    board = chess.Board(fen=fen)
-
-    debug("fen: " + board.fen())
-    moves = list(board.legal_moves)
-
-    # Get best movement
-    (best, m) = minimax(board, 3, board.turn, -99999999, 99999999)
-    debug(f"Best played {best}")
-
-    print(m)
+    x.cycle()
