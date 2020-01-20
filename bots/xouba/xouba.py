962c670fcd379963d56b8740e2e3e60625afcee3
fortes23 <brais.fortes@gmail.com>
2020-01-20 20:30:26 +0100

    bots: Xouba reads weight matrix from json

--- bots/xouba/xouba.py
+++ bots/xouba/xouba.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import json
 
 import chess
 
@@ -11,76 +12,16 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../../xadrez')
 from fish import Fish  # noqa: E402
 
 
-pawn_weight_pos = [0, 0, 0, 0, 0, 0, 0, 0,
-                   5, 10, 10, -20, -20, 10, 10, 5,
-                   5, -5, -10, 0, 0, -10, -5, 5,
-                   0, 0, 0, 20, 20, 0, 0, 0,
-                   5, 5, 10, 25, 25, 10, 5, 5,
-                   10, 10, 20, 30, 30, 20, 10, 10,
-                   50, 50, 50, 50, 50, 50, 50, 50,
-                   0, 0, 0, 0, 0, 0, 0, 0]
-
-knights_weight_pos = [-50, -40, -30, -30, -30, -30, -40, -50,
-                      -40, -20, 0, 5, 5, 0, -20, -40,
-                      -30, 5, 10, 15, 15, 10, 5, -30,
-                      -30, 0, 15, 20, 20, 15, 0, -30,
-                      -30, 5, 15, 20, 20, 15, 5, -30,
-                      -30, 0, 10, 15, 15, 10, 0, -30,
-                      -40, -20, 0, 0, 0, 0, -20, -40,
-                      -50, -40, -30, -30, -30, -30, -40, -50]
-
-bishops_weight_pos = [-20, -10, -10, -10, -10, -10, -10, -20,
-                      -10, 5, 0, 0, 0, 0, 5, -10,
-                      -10, 10, 10, 10, 10, 10, 10, -10,
-                      -10, 0, 10, 10, 10, 10, 0, -10,
-                      -10, 5, 5, 10, 10, 5, 5, -10,
-                      -10, 0, 5, 10, 10, 5, 0, -10,
-                      -10, 0, 0, 0, 0, 0, 0, -10,
-                      -20, -10, -10, -10, -10, -10, -10, -20]
-
-rooks_weight_pos = [0, 0, 0, 5, 5, 0, 0, 0,
-                    -5, 0, 0, 0, 0, 0, 0, -5,
-                    -5, 0, 0, 0, 0, 0, 0, -5,
-                    -5, 0, 0, 0, 0, 0, 0, -5,
-                    -5, 0, 0, 0, 0, 0, 0, -5,
-                    -5, 0, 0, 0, 0, 0, 0, -5,
-                    5, 10, 10, 10, 10, 10, 10, 5,
-                    0, 0, 0, 0, 0, 0, 0, 0]
-
-queens_weight_pos = [-20, -10, -10, -5, -5, -10, -10, -20,
-                     -10, 0, 0, 0, 0, 0, 0, -10,
-                     -10, 5, 5, 5, 5, 5, 0, -10,
-                     0, 0, 5, 5, 5, 5, 0, -5,
-                     -5, 0, 5, 5, 5, 5, 0, -5,
-                     -10, 0, 5, 5, 5, 5, 0, -10,
-                     -10, 0, 0, 0, 0, 0, 0, -10,
-                     -20, -10, -10, -5, -5, -10, -10, -20]
-
-kings_weight_pos = [20, 30, 10, 0, 0, 10, 30, 20,
-                    20, 20, 0, 0, 0, 0, 20, 20,
-                    -10, -20, -20, -20, -20, -20, -20, -10,
-                    -20, -30, -30, -40, -40, -30, -30, -20,
-                    -30, -40, -40, -50, -50, -40, -40, -30,
-                    -30, -40, -40, -50, -50, -40, -40, -30,
-                    -30, -40, -40, -50, -50, -40, -40, -30,
-                    -30, -40, -40, -50, -50, -40, -40, -30]
-
-
 class Xouba(Fish):
-    def __init__(self, depth):
+    def __init__(self, json_file, depth):
         super().__init__(depth, "Starting xouba!")
 
-        self.weight_pieces = {
-            'p': {"mat": 10, "pos": pawn_weight_pos},
-            'n': {"mat": 30, "pos": knights_weight_pos},
-            'b': {"mat": 30, "pos": bishops_weight_pos},
-            'r': {"mat": 50, "pos": rooks_weight_pos},
-            'q': {"mat": 90, "pos": queens_weight_pos},
-            'k': {"mat": 900, "pos": kings_weight_pos}
-        }
+        with open(json_file, "r") as f:
+            jsf = json.load(f)
+            self.weight_pieces = jsf
 
     def get_weigth_piece(self, name, pos):
-        return self.weight_pieces[name]['mat'] + self.weight_pieces[name]['pos'][pos]
+        return self.weight_pieces[name]['wmat'] + self.weight_pieces[name]['wpos'][pos]
 
     def eval_fn(self, board):
         res = 0
@@ -93,6 +34,6 @@ class Xouba(Fish):
         return res
 
 
-x = Xouba(3)
+x = Xouba(os.path.dirname(os.path.realpath(__file__)) + '/xouba.json', 3)
 while True:
     x.cycle()
